554d3e479557ba276387f01e4e00a619695bc41f
Srika1802 <sigicherlasrikanth1802@gmail.com>
2026-02-04 11:25:24 -0800

    Add instance counting script in Total_Instance.tcl
    Adds a script to count total instances and logic instances excluding physical cells.

---
+++ Total_Instance.tcl
@@ -0,0 +1,12 @@
+# Task 1 Total count of instances and Total count of instances with excluding the physical cells
+puts "                                           "
+puts "Task 1 Total count of instances and Total count of instances with excluding the physical cells"
+set insts [get_db insts]
+set logic_insts [get_db insts -if { !.is_physical }]
+
+set inst_count [llength $insts]
+set logic_count [llength $logic_insts]
+
+puts "Total count of instances $inst_count "
+puts "Total count of logic instances $logic_count"
+puts "                                               "
